d6a180dec9f8b883658d01e3cec5cfec6871a05b
AeroRunner <airplay1x6@gmail.com>
2024-01-31 19:40:45 +0300

    done

---
+++ src/test/java/ApiTests.java
@@ -0,0 +1,100 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+
+public class ApiTests {
+    DataTest data = new DataTest();
+
+    @Test
+    @Tag("api")
+    @DisplayName("Create new user")
+    void apiCreateNewUserTest() {
+
+        given()
+                .baseUri(data.URI).contentType(ContentType.JSON)
+                .body(data.user1)
+                .when()
+                .log().all()
+                .post("/api/users")
+                .then()
+                .assertThat()
+                .statusCode(201)
+                .log().all()
+                .body("name", is("Dmitrii"))
+                .body("job", is("Engineer"));
+
+    }
+
+    @Test
+    @Tag("api")
+    @DisplayName("check code 404")
+    void apiCheckErrorCodeTest() {
+        given()
+                .baseUri(data.URI)
+                .when()
+                .log().all()
+                .get("/api/unknown/23")
+                .then()
+                .assertThat()
+                .log().all()
+                .statusCode(404);
+    }
+
+    @Test
+    @Tag("api")
+    @DisplayName("Delete User")
+    void apiDeleteUserTest() {
+        given()
+                .baseUri(data.URI)
+                .when()
+                .log().all()
+                .delete("/api/users/2")
+                .then()
+                .assertThat()
+                .log().all()
+                .statusCode(204);
+    }
+
+    @Test
+    @Tag("api")
+    @DisplayName("Update user")
+    void apiUpdateUserTest() {
+        given()
+                .baseUri(data.URI).contentType(ContentType.JSON)
+                .body(data.user1Up)
+                .when()
+                .log().all()
+                .put("/api/users/2")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .log().all()
+                .body("name", is("Dmitrii Elizarov"))
+                .body("job", is("Senior Engineer"));
+    }
+
+    @Test
+    @Tag("api")
+    @DisplayName("Registration Test")
+    void apiRegisterEmailTest() {
+        given()
+                .baseUri(data.URI).contentType(ContentType.JSON)
+                .body(data.registr)
+                .when()
+                .log().all()
+                .post("/api/register")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .log().all()
+                .body("id", is(4))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+
+    }
+
+}
---
+++ src/test/java/DataTest.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class DataTest {
+    public String URI = "https://reqres.in/";
+    public File user1 = new File("src/test/resources/users/users1.json");
+    public File user1Up = new File("src/test/resources/users/user1Update.json");
+    public File registr = new File("src/test/resources/users/registration.json");
+}
