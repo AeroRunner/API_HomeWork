ddc287ba70d8c3945abdb59b320cb130426ae3ee
AeroRunner <148767698+AeroRunner@users.noreply.github.com>
2024-02-01 03:10:38 +0300

    Update ApiTests.java

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -2,7 +2,6 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
